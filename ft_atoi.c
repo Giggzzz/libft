1b5e3c8d673bc85f6507ada76a20e1937a3bbbb5
Guilhaume Dias <gudias@c2r5s9.42lausanne.ch>
2021-10-22 15:31:35 +0200

    added lasts functions

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,19 +6,20 @@
 /*   By: gudias <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 13:16:32 by gudias            #+#    #+#             */
-/*   Updated: 2021/10/15 17:52:23 by gudias           ###   ########.fr       */
+/*   Updated: 2021/10/22 12:21:23 by gudias           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include<stdio.h>
+#include<stdio.h>
 #include "libft.h"
-//#include<stdlib.h>
+#include<stdlib.h>
+#include<limits.h>
 
 int	ft_atoi(const char *str)
 {
 	int	i;
 	int	minus;
-	int	num;
+	long int	num;
 
 	minus = 1;
 	num = 0;
@@ -34,16 +35,19 @@ int	ft_atoi(const char *str)
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		num = (num * 10) + str[i] - '0';
+		if (num * minus > INT_MAX)
+			return (-1);
+		else if (num * minus < INT_MIN)
+			return (0);
 		i++;
 	}
-	return (num * minus);
+	return ((num * minus));
 }
 
-/*int	main(void)
+int	main(void)
 {
-	char s[] = "       2147483647";
-	if (ft_atoi(s) != atoi(s))
-		printf("%i\n", ft_atoi(s));
+	char s[] = "       -2143928375934864989675448674986774836493";
+	printf("atoi: %d ft_atoi: %d\n", atoi(s), ft_atoi(s));
 	
 	return (0);
-}*/
+}
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,9 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gudias <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/22 12:23:38 by gudias            #+#    #+#             */
+/*   Updated: 2021/10/22 15:26:01 by gudias           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
 
 char    *ft_strmapi(char const *str, char (*f)(unsigned int, char))
 {
-  char  *newstring;
+	char  *newstring;
 
-  newstring = ft_strdup(str);
-  ft_striteri(newstring, f)
-  return (newstring);
+	if (!str || !f)
+		return (NULL);
+	newstring = ft_strdup(str);
+	ft_striteri(newstring, (void*)f);
+	/*i = 0;
+	while (str[i])
+	{
+		newstring[i] = f(i, str[i]);
+		i++;
+	}
+	str[i] = '\0';*/
+	return (newstring);
 }
